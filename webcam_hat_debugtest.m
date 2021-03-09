f8232f8b46b4125331af08ffd1524e784535c384
HasibaZandi <HasibaZandi@localhost>
2021-03-08 23:26:47 -0500

    maybe this will move it to main? -H

---
+++ webcam_hat_debugtest.m
@@ -0,0 +1,54 @@
+function hat()
+% Create the face detector object.
+faceDetector = vision.CascadeObjectDetector('ClassificationModel', 'FrontalFaceLBP');
+
+% Create the webcam object.
+cam = webcam();
+
+% Capture one frame to get its size.
+videoFrame = snapshot(cam);
+frameSize = size(videoFrame);
+
+% Create the video player object.
+videoPlayer = vision.VideoPlayer('Position', [100 100 [frameSize(2), frameSize(1)]+30]);
+
+runLoop = true;
+%numPts = 0;
+frameCount = 0;
+%runLoop &&
+while frameCount < 1000
+    
+    % Get the next frame.
+    videoFrame = snapshot(cam);
+    videoFrameGray = rgb2gray(videoFrame);
+    frameCount = frameCount + 1;
+    % Detection mode.
+    bbox = faceDetector.step(videoFrameGray);
+    
+      % Display the annotated video frame using the video player object.
+    %   step(videoPlayer, videoFrame);
+    %hold on    
+    if ~isempty(bbox)
+        videodouble = im2double(videoFrame);
+        x = zeros(size(videodouble));
+        image(videodouble);
+        gradcap = im2double(imread('../images/grad_cap_neon.png'));
+        % Resize sunglasses image. Set sunglasses to detected eye box ocation
+        hold on
+        gradcap = imresize(gradcap,[bbox(2) (bbox(3) + 1)]);
+        x(1:bbox(2),bbox(1):bbox(1) + bbox(3), :) = gradcap;
+        % Transparency & Overlay of Sunglasses on Guy Image
+        im = image(x);
+        im.AlphaData = max(x, [], 3);
+        hold off
+
+    end   
+      pause(.1);
+        % Check whether the video player window has been closed.
+      %  runLoop = isOpen(videoPlayer);
+    end
+    % Clean up.
+    clear cam;
+    release(videoPlayer);
+    release(faceDetector);
+end
---
+++ webcam_sunglasses.m
@@ -0,0 +1,52 @@
+function sunglasses()
+eyesdetector = vision.CascadeObjectDetector('ClassificationModel', 'EyePairBig');
+cam = webcam();
+
+videoFrame = snapshot(cam);
+frameSize = size(videoFrame);
+videoPlayer = vision.VideoPlayer('Position', [100 100 [frameSize(2), frameSize(1)]+30]);
+
+runLoop = true;
+frameCount = 0;
+
+
+while runLoop && frameCount < 1000
+    % Get the next frame.
+    videoFrame = snapshot(cam);
+    videoFrameGray = rgb2gray(videoFrame);
+    frameCount = frameCount + 1;
+    
+    bboxes = eyesdetector.step(videoFrame);
+    % Places box over where eyes are detected
+    
+    if isempty(bboxes)
+        return
+    end
+    % Bringing in the sunglasses and person photos
+    videodouble = im2double(videoFrame);
+    x = zeros(size(videodouble));
+    image(videodouble);
+    sunglasses = im2double(imread('../images/glasses.png'));
+
+    % Resize sunglasses image. Set sunglasses to detected eye box ocation
+    hold on
+    sunglasses = imresize(sunglasses, [(bboxes(4) + 1) (bboxes(3) + 1)]);
+    x(bboxes(2):bboxes(2) + bboxes(4), bboxes(1):bboxes(1) + bboxes(3), :) = sunglasses;
+    
+    % Transparency & Overlay of Sunglasses on Person
+    im = image(x);
+    im.AlphaData = max(x, [], 3);
+    hold off
+
+    
+    % Display the annotated video frame using the video player object.
+  
+   % step(videoPlayer, x);
+     pause(.1);
+   % runLoop = isOpen(videoPlayer);
+  
+end
+clear cam;
+release(videoPlayer);
+release(eyesdetector);
+end
